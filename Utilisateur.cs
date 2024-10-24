34b1f6fd6f83ded36c0751c9ab6f9a9fe0aa8fc5
Hertz Sully Djihoua <300139241@monboreal.ca>
2024-10-23 22:30:56 -0400

    Étape 6: Creation de la classe Utilisateur

---
+++ Utilisateur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        //Propriété
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+
+
+        //Constructeur
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            Paiements = new List<Paiement>();
+        }
+
+
+        //methode afficher infos
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Nom:{Nom}");
+            foreach (Paiement p in Paiements)
+            {
+                p.AfficherDetail();
+            }
+        }
+        //méthode ajouter paiements
+        public void AjouterPaiement(Paiement paiement)
+        {
+            Paiements.Add(paiement);
+        }
+    }
+}
