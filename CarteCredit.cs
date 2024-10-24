766ef9b646b39a7964ffc5afc1c5a2664b2ebdf5
Hertz Sully Djihoua <300139241@monboreal.ca>
2024-10-23 22:03:28 -0400

    Etape 4: Création de la classe CarteCredit dans dev-cartecredit

---
+++ CarteCredit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit:Paiement
+    {
+        // Les propriété
+        public int NumeroCarte { get; set; }
+
+        // Le constructeur
+        public CarteCredit(int carte, string description, double montant) : base(montant, description)
+        {
+            NumeroCarte = carte;
+        }
+
+        //La méthode Afficher
+        public override void AfficherDetail()
+        {
+            Console.WriteLine($"Description: {Description}, Montant:{Montant}, NumeroCarte:{NumeroCarte}");
+        }
+
+    }
+}
